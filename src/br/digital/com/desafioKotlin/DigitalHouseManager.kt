90f3d4d8b20c16ad79b48b5ed4ee704f26d22791
italohmsilva <italohmsilva@gmail.com>
2020-09-26 12:45:33 -0300

    STEP G: criando classe DigitalHouseManager

---
+++ src/br/digital/com/desafioKotlin/DigitalHouseManager.kt
@@ -0,0 +1,9 @@
+package br.digital.com.desafioKotlin
+
+class DigitalHouseManager(
+        var listaAlunos: MutableList<Aluno>,
+        var listaProfessor: MutableList<Professor>,
+        var listaCursos: MutableList<Curso>,
+        var listaMatricuylas: MutableList<Matricula>
+) {
+}
